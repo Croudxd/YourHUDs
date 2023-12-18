a57a69d1d7675d4248749a6964f2e4ead6c2b73a
Croudxd <benjamincroud@gmail.com>
2023-12-18 22:57:19 +0000

    The image functions are almost working, just a few small bugs to sort out. Also started working on the download function using QNetwork.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,13 +2,22 @@
 #include "./ui_mainwindow.h"
 #include "hud.h"
 #include <iostream>
+#include <QFile>
+#include <QDir>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QUrl>
+
+
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    connect(ui->button7Hud, SIGNAL(clicked()), this, SLOT(on_button7Hud_clicked()));
+    connect(ui->button7Hud, &QPushButton::clicked, this, &MainWindow::on_button7Hud_clicked);
 
 }
 MainWindow::~MainWindow()
@@ -20,17 +29,41 @@ MainWindow::~MainWindow()
 void MainWindow::on_installbutton_clicked()
 {
     QString link = currentHud->getDownloadLink();
+
+    QNetworkAccessManager* manager = new QNetworkAccessManager(this);
+    QNetworkRequest request(QUrl(link));
+
+    QNetworkReply* reply = manager->get(request);
+
+    connect(reply, &QNetworkReply::finished, [=]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            QByteArray data = reply->readAll();
+
+            // Save the downloaded data to a file
+            QString fileName = "downloaded_file.zip";  // Set your desired file name
+            QFile file(fileName);
+            if (file.open(QIODevice::WriteOnly)) {
+                file.write(data);
+                file.close();
+                qDebug() << "File downloaded successfully: " << fileName;
+            } else {
+                qDebug() << "Failed to save file.";
+            }
+        } else {
+            qDebug() << "Download failed: " << reply->errorString();
+        }
+
+        reply->deleteLater();
+        manager->deleteLater();
+    });
 }
 
 std::vector<QString> MainWindow::createImagesVector(QString fileName)
 {
-
     std::vector<QString> imagePaths;
-    //retreieve images from hud file.
-    QString path = ":/imgs/";
-    path += fileName + "/";
+    QString path = ":/imgs/imgs/" + fileName + "/";
 
-    for (int i = 1; i < 5 ; i++)
+    for (int i = 1; i <= 4; ++i)
     {
         QString imgPath = path + "image" + QString::number(i) + ".jpg";
         imagePaths.push_back(imgPath);
@@ -44,6 +77,7 @@ std::vector<QString> MainWindow::createImagesVector(QString fileName)
 
 
 void MainWindow::setHud(QString name, QString description, QString creator, QString downloadlink){
+
     currentHud = new hud();
     currentHud->setName(name);
     currentHud->setDescription(description);
@@ -53,7 +87,13 @@ void MainWindow::setHud(QString name, QString description, QString creator, QStr
 }
 void MainWindow::setImages(QLabel *label, const QString imgPath)
 {
+    label->clear();
     QPixmap pixmap(imgPath);
+    if (pixmap.isNull()) {
+        qDebug() << "Image loading failed!";
+    } else {
+        qDebug() << "Image loaded successfully.";
+    }
     label->setPixmap(pixmap);
     label->setScaledContents(true);
 }
@@ -62,21 +102,18 @@ void MainWindow::setImages(QLabel *label, const QString imgPath)
 
 void MainWindow::on_button7Hud_clicked()
 {
-
+    std::cout << "Hello" << std::endl;
     setHud("FlawHUD", "FlawHUD Custom hud", "CriticalFlaw", "https://github.com/CriticalFlaw/flawhud");
-    std::cout << "Calling createImagesVector" << std::endl;
     std::vector<QString> imgPaths = createImagesVector("FlawHUD");
     setImages(ui->label_2, imgPaths[0]);
     currentHud->setImages(imgPaths);
-    for(const QString &img : imgPaths){
-        qDebug() << img;
-    }
-
-
 }
 void MainWindow::on_buttonBudHUD_clicked()
 {
     setHud("BudHUD", "BudHUD Custom HUD", "rbJaxter", "https://github.com/rbjaxter/budhud");
+    std::vector<QString> imgPaths = createImagesVector("BudHUD");
+    setImages(ui->label_2, imgPaths[0]);
+    currentHud->setImages(imgPaths);
 }
 
 
